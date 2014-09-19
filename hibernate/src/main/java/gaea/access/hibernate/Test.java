1de053881f3f767c930e6caa22c0f3e0044c7f0f
Jorson <iamgoingin123@163.com>
2014-09-19 22:00:42 +0800

    创建Gaea的文件结构，并实现Core的代码

---
+++ hibernate/src/main/java/gaea/access/hibernate/Test.java
@@ -0,0 +1,7 @@
+package gaea.access.hibernate;
+
+/**
+ * Created by jorson on 14-9-19.
+ */
+public class Test {
+}
---
+++ mongodb/src/test/java/Test.java
@@ -0,0 +1,5 @@
+/**
+ * Created by jorson on 14-9-19.
+ */
+public class Test {
+}
